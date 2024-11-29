0e0942f56c1f66499e4926539d44c7bc4b426633
besbesbesbes <smt.bes@gmail.com>
2024-11-29 17:11:49 +0700

    Add sum number for trans

--- src/components/Trans/TransMainDetail.jsx
+++ src/components/Trans/TransMainDetail.jsx
@@ -38,14 +38,41 @@ function TransMainDetail({ tran }) {
     </div>
   ));
 
+  const formatNumber = (value, type) => {
+    if (value === 0) {
+      return ""; // Return empty string for zero
+    }
+
+    const absValue = Math.abs(value).toLocaleString("en-US", {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    });
+
+    if (type === "minus") {
+      return `-${absValue}`; // Prefix with a minus for "minus" type
+    } else {
+      return `+${absValue}`; // No prefix for "plus" type
+    }
+  };
+
   return (
     <div
       className="w-full min-h-[30px] px-1 bg-prim-6 flex items-center cursor-pointer my-1 rounded-full"
       onClick={hdlShowDetail}
     >
-      <div className="w-full flex flex-wrap">
-        <Tag icon={icon} txt={txt} color={color} isShowTxt={isShowTxt} />
-        {renderTransSub}
+      <div className="w-full flex items-center">
+        <div className="w-full flex flex-wrap ">
+          <Tag icon={icon} txt={txt} color={color} isShowTxt={isShowTxt} />
+          {renderTransSub}
+        </div>
+        <div className="min-w-[140px] flex justify-between px-1 font-bold">
+          <div className="text-acct-2">
+            {formatNumber(tran.transSubIsInTrue, "plus")}
+          </div>
+          <div className="text-acct-7">
+            {formatNumber(tran.transSubIsInFalse, "minus")}
+          </div>
+        </div>
       </div>
     </div>
   );
